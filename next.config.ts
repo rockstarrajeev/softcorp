52aa6fa3f63252ec64b2997c9c51007ad14d8508
Rajeev <rajeev@wesoftcorp.com>
2026-03-18 17:36:58 +0530

    Scrap Sanity integration and restore original site

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  swcMinify: false,
-  output: "standalone",
+  /* config options here */
 };
 
 export default nextConfig;
