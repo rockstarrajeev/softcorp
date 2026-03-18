664f9545389d1bb73b4d1be35332eb44426b1d3d
Rajeev <rajeev@wesoftcorp.com>
2026-03-18 19:10:18 +0530

    Completely remove Sanity UI and dependencies

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { companyInfo } from './companyInfo' // 1. Import it
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [companyInfo], // 2. Add it here
-}
